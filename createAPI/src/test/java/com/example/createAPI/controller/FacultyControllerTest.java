c603e59ca886a8c9b1dfcca5f6d69b10a22342ce
Андрей <andrei-loktev@yandex.ru>
2023-10-01 15:24:46 +0300

    test rest template выполнен

--- createAPI/src/test/java/com/example/createAPI/controller/FacultyControllerTest.java
+++ createAPI/src/test/java/com/example/createAPI/controller/FacultyControllerTest.java
@@ -18,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest(classes = CreateApiApplication.class,
-webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FacultyControllerTest {
 
     @Autowired
