8f62eda22f461eaad5d2ef724be9f815dfe6b1bc
Андрей <andrei-loktev@yandex.ru>
2023-10-05 14:25:55 +0300

    дз 4.2 отредактировано

--- createAPI/Scripts422.sql
+++ createAPI/Scripts422.sql
@@ -1,3 +1,40 @@
+--Описание структуры: у каждого человека есть машина. Причем несколько человек могут пользоваться одной машиной.
+--У каждого человека есть имя, возраст и признак того, что у него есть права (или их нет).
+--У каждой машины есть марка, модель и стоимость.
+--Также не забудьте добавить таблицам первичные ключи и связать их.
+
+create table car (
+id serial primary key,
+brand varchar,
+model varchar,
+price int
+)
+
+create table driver (
+id serial primary key,
+name varchar,
+age int,
+license boolean,
+car_id int references car (id)
+)
+
+insert into car(brand, model, price) values ('toyota','raize',1000);
+insert into car(brand, model, price) values ('daihatsu','rocky',1500);
+insert into car(brand, model, price) values ('honda','shuttle',500);
+
+insert into driver(name, age, license, car_id) values ('katya',20,true, 1);
+insert into driver(name, age, license, car_id) values ('vika',22,true, 2);
+insert into driver(name, age, license, car_id) values ('olya',24,false, 1);
+insert into driver(name, age, license, car_id) values ('tanya',26,false , 3);
+
+select * from car;
+
+select * from driver;
+
+select * from car c
+inner join driver d on c.id = d.car_id;
+
+
 --Составить первый JOIN-запрос, чтобы получить информацию обо всех студентах
 --(достаточно получить только имя и возраст студента) школы Хогвартс вместе с названиями факультетов.
 
@@ -15,4 +52,4 @@ select student_id from avatar
 group by student_id;
 
 select * from student s
-left join avatar a on s.id = a.id;
+inner join avatar a on s.id = a.id;
